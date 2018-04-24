540f807769b3ea6c2fabcf1c9e5bd1067a2f450a
A.Sofiane Guerfi <schguerfi@gmail.com>
2018-04-24 12:20:12 -0700

    UI_addEvent

--- testcontract.sol
+++ testcontract.sol
@@ -4,10 +4,16 @@ contract Coursetro {
     
    string fName;
    uint age;
+//setting an event listener for an automated refresh
+   event Instructor(
+        string name,
+        uint age
+    );
    
    function setInstructor(string _fName, uint _age) public {
        fName = _fName;
        age = _age;
+       Instructor(_fName, _age);
    }
    
    function getInstructor() public constant returns (string, uint) {
